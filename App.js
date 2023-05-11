2d53533d94fc0cbbc22e47f9a9abb350c69e41b3
guilherme <guiduffes@gmail.com>
2023-05-11 00:13:11 -0300

    commit

--- App.js
+++ App.js
@@ -1,20 +1,107 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, TextInput, View, TouchableOpacity } from 'react-native';
+import React, {useState} from 'react'
 export default function App() {
+
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+  const [result, setResult] = useState();
+
+  const n1 = parseInt(number1);
+  const n2 = parseInt(number2);
+
+  function plus(){
+    const result = n1 + n2;
+    setResult(result.toString());
+  }
+
+  function less(){
+    const result = n1 - n2;
+    setResult(result.toString());
+  }
+
+  function multiply(){
+    const result = n1 * n2;
+    setResult(result.toString());
+  }
+
+  function divide(){
+    const result = n1 / n2;
+    setResult(result.toString());
+  }
+
+  
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      <Text>Calculator</Text>
+      <View> 
+        <Text style={styles.Number} >Number 1:</Text>
+        <TextInput
+        style={styles.Input}
+        placeholder='0'
+        keyboardType='numeric'
+        onChangeText={setNumber1}
+        />        
+      </View>
+
+      <View> 
+        <Text style={styles.Number} >Number 2:</Text>
+        <TextInput
+        style={styles.Input}
+        placeholder='0'
+        keyboardType='numeric'
+        onChangeText={setNumber2}
+
+        />        
+      </View>
+
+      <View>
+        <TouchableOpacity onPress={plus} style={styles.button} ><Text>+</Text></TouchableOpacity>
+        <TouchableOpacity onPress={less} style={styles.button} ><Text>-</Text></TouchableOpacity>
+        <TouchableOpacity onPress={multiply} style={styles.button} ><Text>*</Text></TouchableOpacity>
+        <TouchableOpacity onPress={divide} style={styles.button} ><Text>/</Text></TouchableOpacity>
+      </View>
+
+      <View> 
+        <Text style={styles.Number} >Result</Text>
+        <TextInput
+        style={styles.Input}
+        placeholder='0'
+        keyboardType='numeric'
+        value={result}
+        editable={false}
+
+        />        
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  Input: {
+    borderBottomWidth: 1,
+    borderBottomColor: "black",
+    width: 70,
+    color: "#000000",
   },
+  Number: {
+    backgroundColor: "#d3d3d3",
+    width: 70,
+    color: "#000000",
+  },
+  button: {
+    width: 10,
+    backgroundColor: "black"
+  },
+  button: {
+    backgroundColor: '#2196F3',
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+    width: 100,
+    marginBottom: 10,
+    marginTop: 5,
+  }
 });
